9e7f93eede819f35c26bf38e6b6a34ca7342fe58
Abhishe <abhisheknarwariya813@gmail.com>
2022-03-25 17:01:31 +0530

    Exact100

--- Exact100/Program.cs
+++ Exact100/Program.cs
@@ -10,38 +10,53 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-           
+            
             int rollDice, playerPos = 0;
+            int count = 0;
 
          
             Random random = new Random();
-        
+       
             var list = new List<string> { "No Play", "Ladder", "Snake" };
 
-            
+         
             rollDice = random.Next(1, 7);
             Console.WriteLine("Dice number is : " + rollDice);
 
-            
+          
             Console.WriteLine("Player checks options 1.No Play  2.Ladder   3.Snake");
 
-           
-            int index = random.Next(list.Count);
-            Console.WriteLine("Player option is : " + list[index]);
+          
+            int pos = random.Next(list.Count);
+            Console.WriteLine("Player option is : " + list[pos]);
             if (playerPos + rollDice < 100)
             {
-                if (list[index] == "Ladder") playerPos += rollDice;
-                if (list[index] == "Snake") playerPos -= rollDice;
+                if (list[pos] == "Ladder")
+                {
+                    playerPos += rollDice;
+                }
+                if (list[pos] == "Snake")
+                {
+                    playerPos -= rollDice;
+                }
+            }
+            else if (playerPos + rollDice == 100 && list[pos] == "Ladder")
+            {
+                playerPos += rollDice;
+
             }
 
             if (playerPos < 0)
             {
                 playerPos = 0;
             }
-           
+       
             Console.WriteLine("\n");
             Console.WriteLine("Player current position : " + playerPos);
+            count++;
             Console.WriteLine("Final position is :" + playerPos);
+            Console.WriteLine("Total MOves:" + count);
         }
+
     }
 }
